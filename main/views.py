fcdd4a3dde2b3dc24cb8019b9656b988009b94a2
NaufalAul <auliactivity@gmail.com>
2023-11-22 16:24:04 +0700

    Fix url for main

--- main/views.py
+++ main/views.py
@@ -98,12 +98,12 @@ def login_user(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
-            if user is not None:
-                login(request, user)
-                response = HttpResponseRedirect(reverse("main:show_main")) 
-                response.set_cookie('last_login', str(datetime.datetime.now()))
-                return response
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
         else:
             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
     context = {}
